664a0cbbc4bf0fa257c2cb6a65c0825548be0342
Glenn Van Mele <glennvanmele@gmail.com>
2024-05-03 11:27:58 +0200

    chore: cleanup ✨

--- index.js
+++ index.js
@@ -5,34 +5,36 @@ const sendMail = require('./mailer')
 
 const start = async () => {
     try {
-        const {generalAdvice, recommendations} = await createRunningRecommendation('Gent', 'Belgium')
+        const {generalAdvice, recommendations = []} = await createWeatherReport('Gent', 'Belgium')
+        
         console.log(`Our recommendation:\n\n${generalAdvice}\n`)
-        if (recommendations) recommendations.forEach(rec => console.log(rec))
+        recommendations.forEach(rec => console.log(rec))
+        
         await sendMail('test@example.com', generalAdvice, recommendations || '')
     } catch (e) {
         console.error(e)
     }
 }
 
-const createRunningRecommendation = async (city = 'Gent', country = 'Belgium') => {
+const createWeatherReport = async (city = 'Gent', country = 'Belgium') => {
     // Get coordinates and weather data
     const { lat, lon } = await api.getCoordinates(city, country)
     const { city: cityData, list: weatherList } = await api.getWeatherData(lat, lon)
 
-    const weather = weatherList.slice(0,5)
+    const predictions = weatherList.slice(0,5)
     const timezoneOffset = cityData.timezone
 
-    return getRecommendation(weather, timezoneOffset)
+    return getRecommendationsNext15Hours(predictions, timezoneOffset)
 }
 
-const getRecommendation = (predictions, timezoneOffset) => {
+const getRecommendationsNext15Hours = (predictions, timezoneOffset) => {
     const { max: maxTemp, min: minTemp } = getExtremaTemperatures(predictions)
     if (maxTemp < 5) return { generalAdvice: `It's too cold ❄️, hit the threadmill!` }
     if (minTemp > 25) return { generalAdvice: `It's too hot 🥵, hit the threadmill!` }
 
     const goodWeatherConditions = filterGoodWeatherConditions(predictions)
+    
     if (goodWeatherConditions.length === 0) return { generalAdvice: 'The weather is terrible, hit the threadmill!' }
-
     return { 
         generalAdvice: '🏃‍♂️‍➡️ Run, Forest! Run! 🏃‍♂️‍➡️',
         recommendations: parseGoodWeatherRecommendations(goodWeatherConditions, timezoneOffset) 
@@ -53,26 +55,27 @@ const parseGoodWeatherRecommendations = (conditions, timezoneOffset) => {
         const timestamp = (condition.dt - timezoneOffset) * 1000
         const startTime = format(timestamp, 'HH:mm')
         const endTime = format(addHours(timestamp, 3), 'HH:mm')
-        
-        const weatherType = condition.weather[0].id
+        const timeWindow = `- ${startTime} - ${endTime}: `
 
-        let recommendation = ''
-
-        if (weatherType >= 300 && weatherType <= 399) {
-            recommendation = `- ${startTime} - ${endTime}: 🌧️there might be some drizzle , maybe bring a jacket`
-        }
-        if (weatherType >= 500 && weatherType <= 502) {
-            recommendation = `- ${startTime} - ${endTime}: 🌧️ there might be some light rain , probably a good idea to bring a jacket`
-        }
-        if (weatherType === 800) {
-            recommendation = `- ${startTime} - ${endTime}: ☀️ it's clear, enjoy!`
-        }
-        if (weatherType >= 800 && weatherType <= 899) {
-            recommendation = `- ${startTime} - ${endTime}: ⛅️ it might be a bit cloudy`
-        }
-        console.log(condition)
-        return `${recommendation}\n 🌡️  ${condition.main.temp}°C - 💨 ${condition.wind.speed}km/h\n`
+        const advice = parseAdvice(condition.weather[0].id)
+        return `${timeWindow}${advice}\n 🌡️  ${condition.main.temp}°C - 💨 ${condition.wind.speed}km/h\n`
     })
+}
+
+const parseAdvice = (weatherType) => {
+    if (weatherType >= 300 && weatherType <= 399) {
+        return "🌧️ there might be some drizzle, maybe bring a jacket"
+    }
+    if (weatherType >= 500 && weatherType <= 502) {
+        return "🌧️ there might be some light rain, probably a good idea to bring a jacket"
+    }
+    if (weatherType === 800) {
+        return "☀️ it's clear, enjoy!"
+    }
+    if (weatherType >= 800 && weatherType <= 899) {
+        return "⛅️ it might be a bit cloudy"
+    }
+    return ""
 }
 
 const getExtremaTemperatures = (predictions) => {
